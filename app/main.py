fc94c004e7782ca8b8888672992718d713f78ce9
MariaAless <alessandraaquino2003@gmail.com>
2026-05-08 21:14:50 -0300

    feat(favorites): implement favorites endpoints

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI
 from app.api.routes.auth_test import router as auth_router
 from app.api.routes.product import router as product_router
 from app.api.routes.store import router as store_router
+from app.api.routes.favorite import router as favorite_router
 
 if sys.platform == 'win32':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
@@ -15,6 +16,7 @@ app = FastAPI()
 app.include_router(store_router)
 app.include_router(auth_router)
 app.include_router(product_router)
+app.include_router(favorite_router)
 
 
 @app.get('/')
